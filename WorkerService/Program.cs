3448be110a61a8de4e04a65711dd78f417d5fcb2
ahig0ne <sebaselnub@gmail.com>
2025-06-06 07:49:50 -0500

    feat: :sparkles: readme built

---
+++ WorkerService/Program.cs
@@ -0,0 +1,7 @@
+using MiWorkerService;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ WorkerService/Worker.cs
@@ -0,0 +1,23 @@
+namespace MiWorkerService;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            if (_logger.IsEnabled(LogLevel.Information))
+            {
+                _logger.LogInformation("Hola campers, los saludo en: {time}", DateTimeOffset.Now);
+            }
+            await Task.Delay(3000, stoppingToken);
+        }
+    }
+}
